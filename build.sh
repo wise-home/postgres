fb8ccfdfbbe60bec3a72e5ad2ca541a1523fc6b6
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-12-09 10:43:42 +0100

    Remove postgres 9.6, tweak build script

--- build.sh
+++ build.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-# This script builds the wisehome/postgres_wisehome container image using podman in both versions
-
 set -e
 
 echo ""
-echo "Building postgres_wisehome:9.6"
-podman pull --tls-verify=true docker.io/library/postgres:9.6
-podman build . -t docker.io/wisehome/postgres_wisehome:9.6 -f Containerfile-9.6
-
-echo ""
 echo "Building postgres_wisehome:12"
-podman pull --tls-verify=true docker.io/library/postgres:12
+podman pull docker.io/library/postgres:12
 podman build . -t docker.io/wisehome/postgres_wisehome:12 -f Containerfile-12
 
 echo ""
 echo "Authenticating with Docker Hub and pushing images"
-podman login --tls-verify=true docker.io
-podman push --tls-verify=true docker.io/wisehome/postgres_wisehome:9.6
-podman push --tls-verify=true docker.io/wisehome/postgres_wisehome:12
+podman login docker.io
+podman push docker.io/wisehome/postgres_wisehome:12
 
 echo ""
 echo "All done."
